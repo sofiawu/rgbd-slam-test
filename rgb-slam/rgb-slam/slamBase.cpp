bb4885a99e6f616f334d7d6734dca96fd9deea45
sofiawu <sofiawu@tencent.com>
2017-07-10 18:57:51 +0800

    vo

--- rgb-slam/rgb-slam/slamBase.cpp
+++ rgb-slam/rgb-slam/slamBase.cpp
@@ -54,3 +54,84 @@ cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera
     return p;
 }
 
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame)
+{
+    // 声明特征提取器与描述子提取器
+    cv::Ptr<cv::ORB> detector = cv::ORB::create(800, 1.2f, 8);
+    
+    vector< cv::KeyPoint > kp1, kp2; //关键点
+    cv::Mat desp1, desp2; // 计算描述子
+    detector->detectAndCompute(frame.rgb, cv::noArray(), frame.kp, frame.desp);
+}
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2
+// 输出：rvec 和 tvec
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    static ParameterReader pd;
+    vector< cv::DMatch > matches;
+    cv::BFMatcher matcher;
+    matcher.match( frame1.desp, frame2.desp, matches );
+    
+    cout<<"find total "<<matches.size()<<" matches."<<endl;
+    vector< cv::DMatch > goodMatches;
+    double minDis = 9999;
+    double good_match_threshold = atof( pd.getData( "good_match_threshold" ).c_str() );
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if ( matches[i].distance < minDis )
+            minDis = matches[i].distance;
+    }
+    
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if (matches[i].distance < 10*minDis)
+            goodMatches.push_back( matches[i] );
+    }
+    
+    cout<<"good matches: "<<goodMatches.size()<<endl;
+    // 第一个帧的三维点
+    vector<cv::Point3f> pts_obj;
+    // 第二个帧的图像点
+    vector< cv::Point2f > pts_img;
+    
+    // 相机内参
+    for (size_t i=0; i<goodMatches.size(); i++)
+    {
+        // query 是第一个, train 是第二个
+        cv::Point2f p = frame1.kp[goodMatches[i].queryIdx].pt;
+        // 获取d是要小心！x是向右的，y是向下的，所以y才是行，x是列！
+        ushort d = frame1.depth.ptr<ushort>( int(p.y) )[ int(p.x) ];
+        if (d == 0)
+            continue;
+        pts_img.push_back( cv::Point2f( frame2.kp[goodMatches[i].trainIdx].pt ) );
+        
+        // 将(u,v,d)转成(x,y,z)
+        cv::Point3f pt ( p.x, p.y, d );
+        cv::Point3f pd = point2dTo3d( pt, camera );
+        pts_obj.push_back( pd );
+    }
+    
+    float camera_matrix_data[3][3] = {
+        {camera.fx, 0, camera.cx},
+        {0, camera.fy, camera.cy},
+        {0, 0, 1}
+    };
+    
+    cout<<"solving pnp"<<endl;
+    // 构建相机矩阵
+    cv::Mat cameraMatrix( 3, 3, CV_32F, camera_matrix_data );
+    cv::Mat rvec, tvec, inliers;
+    // 求解pnp
+    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 0.99, inliers );
+    
+    RESULT_OF_PNP result;
+    result.rvec = rvec;
+    result.tvec = tvec;
+    result.inliers = inliers.rows;
+    
+    return result;
+}
+
--- rgb-slam/rgb-slam/slamBase.hpp
+++ rgb-slam/rgb-slam/slamBase.hpp
@@ -13,11 +13,12 @@
 // C++标准库
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 // OpenCV
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 //PCL
 #include <pcl/io/pcd_io.h>
@@ -30,7 +31,22 @@ typedef pcl::PointCloud<PointT> PointCloud;
 // 相机内参结构
 struct CAMERA_INTRINSIC_PARAMETERS
 {
-    double cx, cy, fx, fy, scale;
+    float cx, cy, fx, fy, scale;
+};
+
+// 帧结构
+struct FRAME
+{
+    cv::Mat rgb, depth; //该帧对应的彩色图与深度图
+    cv::Mat desp;       //特征描述子
+    vector<cv::KeyPoint> kp; //关键点
+};
+
+// PnP 结果
+struct RESULT_OF_PNP
+{
+    cv::Mat rvec, tvec;
+    int inliers;
 };
 
 // 函数接口
@@ -40,5 +56,59 @@ PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC
 // point2dTo3d 将单个点从图像坐标转换为空间坐标
 // input: 3维点Point3f (u,v,d)
 cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame);
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2, 相机内参
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// 参数读取类
+class ParameterReader
+{
+public:
+    ParameterReader( string filename="./parameters.txt" )
+    {
+        ifstream fin( filename.c_str() );
+        if (!fin)
+        {
+            cerr<<"parameter file does not exist."<<endl;
+            return;
+        }
+        while(!fin.eof())
+        {
+            string str;
+            getline( fin, str );
+            if (str[0] == '#')
+            {
+                // 以‘＃’开头的是注释
+                continue;
+            }
+            
+            int pos = str.find("=");
+            if (pos == -1)
+                continue;
+            string key = str.substr( 0, pos );
+            string value = str.substr( pos+1, str.length() );
+            data[key] = value;
+            
+            if ( !fin.good() )
+                break;
+        }
+    }
+    string getData( string key )
+    {
+        auto iter = data.find(key);
+        if (iter == data.end())
+        {
+            cerr<<"Parameter name "<<key<<" not found!"<<endl;
+            return string("NOT_FOUND");
+        }
+        return iter->second;
+    }
+public:
+    unordered_map<string, string> data;
+};
 
 #endif /* slamBase_hpp */
